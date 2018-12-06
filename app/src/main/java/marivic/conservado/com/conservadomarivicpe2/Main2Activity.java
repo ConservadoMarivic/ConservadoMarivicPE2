c7936d20399935501c7db5532b48dd9797a7a99a
ConservadoMarivic <2015081245@ust-ics.mygbiz.com>
2018-12-06 12:53:15 +0800

    Version 2

---
+++ app/src/main/java/marivic/conservado/com/conservadomarivicpe2/Main2Activity.java
@@ -0,0 +1,21 @@
+package marivic.conservado.com.conservadomarivicpe2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import org.w3c.dom.Text;
+
+public class Main2Activity extends AppCompatActivity {
+
+    EditText fullname_field, age_field, gender_field;
+    Button save_button, search_button;
+
+    Text fullname_result, age_result, gender_result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
