771611b46622141b063c8e46e9d207adc0645a2e
ConservadoMarivic <2015081245@ust-ics.mygbiz.com>
2018-12-06 12:27:41 +0800

    Initial commit

---
+++ app/src/main/java/marivic/conservado/com/conservadomarivicpe2/Record.java
@@ -0,0 +1,35 @@
+package marivic.conservado.com.conservadomarivicpe2;
+
+public class Record {
+
+    String fullname, gender;
+    int age;
+
+    public Record() {
+    }
+
+    public Record(String fullname, String gender, int age) {
+        this.fullname = fullname;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public String getFullname() {return fullname;}
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getGender() {return gender;}
+
+    public void setGender(String fullname){
+        this.gender = gender;
+    }
+
+    public int getAge() {return age;}
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+}
